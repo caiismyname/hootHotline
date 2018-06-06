e62ca2945c982900b317fb716ff1cc011f3490d5
caiismyname <davidcai2012@gmail.com>
2018-06-05 21:31:14 -0700

    Homepage succesfully populates with data from firebase

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-// Express is used to generate the website
-const express = require('express');
-const app = express();
+//
+//
+// Initialization
+//
+//
 
 const bodyParser = require('body-parser');
 const request = require('request');
-// const firebase = require('firebase/app');
-// require('firebase/database');
 const env = require('dotenv').config();
+// Express is used to generate the website
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
 var firebase; // Global reference to Firebase DB. Set in initFirebase().
+
+/**
+ * Dictionary containing ID to food mappings.
+ * Foods are correctly capitalized/spaced.
+ * This functions as an enum of the available foods.
+ */
+const FOODS = {
+    1: "Pizza",
+    2: "Sandwiches"
+}
 
 function initFirebase() {
     // Verifying admin credentials for authenticated access to Firebase
@@ -25,10 +41,52 @@ function initFirebase() {
     });
 }
 
-function test() {
+/**
+ * Starts a reference listening to the inventory DB.
+ * Renders the homepage (with inventory information) when first called.
+ * Refreshes the homepage when updates to the DB occur.
+ */
+
+function watchInventory(res) {
+    const ref = firebase.database().ref("inventory");
+    ref.on('value', function(snapshot) {
+        console.log("Food inventory update");
+        let foodStatuses = snapshot.val();
+        var inStock = [];
+        var outStock = [];
+
+        for (let idx in foodStatuses) {
+            console.log("\t", FOODS[idx], ": ", foodStatuses[idx]);
+            // Put food in appropriate list according to stock status.
+            if (foodStatuses[idx]) {
+                inStock.push(FOODS[idx]);
+            } else {
+                outStock.push(FOODS[idx]);
+            }
+        }
+        res.render('homepage', {inStock: inStock, outStock: outStock});
+        
+    });
+}
+
+//
+//
+// Setting Express endpoints for the pages
+//
+//
+
+
+app.get('/', function (req, res) {
+    watchInventory(res);
+});
+
+function InsertIntoFBTest() {
     var ref = firebase.database().ref("david");
     ref.set("bar");
 }
 
 initFirebase();
-test();
+
+app.listen(3000, function () {
+	console.log('Listening on port 3000!');
+})
